50cf81c0a2d271347e5964a797338184e0752afc
PradyumanAgrawal <pa14@iitbbs.ac.in>
2020-08-13 22:54:17 +0530

    Basic app setup

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'views','index.html'));
 })
 
-app.listen(3000,()=>{
-        console.log('listening on port 3000')
+app.listen(process.env.PORT,()=>{
+        console.log('listening on port'+process.env.PORT)
 })
