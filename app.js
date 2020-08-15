a8d5e5640d0760745508826866d6a33a69d1ddba
PradyumanAgrawal <pa14@iitbbs.ac.in>
2020-08-15 14:56:07 +0530

    bug fix

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'views','index.html'));
 })
 
+app.post("/webhook", (req, res) => {
+    console.log("activate webhook");
+    console.log(req.body);
+    res.status(200).send();
+});
+
 app.listen(process.env.PORT,()=>{
         console.log('listening on port'+process.env.PORT)
 })
 
-//require('./webhook')(app);
