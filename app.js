5c82d7dbf72afc3bbc9a6fc54cb254bcd04a4e26
PradyumanAgrawal <pa14@iitbbs.ac.in>
2020-08-16 16:53:25 +0530

    bug fix

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path= require('path')
 
 const app = express();;
 
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'views','index.html'));
 })
